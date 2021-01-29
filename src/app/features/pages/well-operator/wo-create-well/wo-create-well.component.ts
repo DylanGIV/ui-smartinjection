0531bcb6cf05e9fbace3cab71b34c7a555f4fc73
Angela Tante <theangelat@gmail.com>
2021-01-28 17:54:00 -0800

    building create well form

--- src/app/features/pages/well-operator/wo-create-well/wo-create-well.component.ts
+++ src/app/features/pages/well-operator/wo-create-well/wo-create-well.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-wo-create-well',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WoCreateWellComponent implements OnInit {
 
+  wellForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    
   }
 
 }
