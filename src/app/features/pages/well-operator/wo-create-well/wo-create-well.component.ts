3f41c3af24e215a1ed45a0be4575ff4176025c2a
Angela Tante <theangelat@gmail.com>
2021-01-28 20:36:42 -0800

    made create well a reactive form!

--- src/app/features/pages/well-operator/wo-create-well/wo-create-well.component.ts
+++ src/app/features/pages/well-operator/wo-create-well/wo-create-well.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wo-create-well',
@@ -10,14 +10,41 @@ export class WoCreateWellComponent implements OnInit {
 
   wellForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
   initializeForm(): void {
-    
+    this.wellForm = this.fb.group ({
+      wells: this.fb.array([
+        this.newWell()
+      ])
+    });
+  }
+
+  newWell(): FormGroup {
+    return this.fb.group({
+      wellName: '',
+      lease: '',
+      wellType: '',
+      locationType: '',
+      location: this.fb.group({
+        x: '',
+        y: '',
+        z: ''
+      })
+    });
+  }
+
+  get wells(): FormArray {
+    return this.wellForm.get('wells') as FormArray;
+  }
+
+  onSubmit(): void {
+    console.log(this.wellForm);
+    console.log(this.wellForm.value.wells[0].wellName)
   }
 
 }
